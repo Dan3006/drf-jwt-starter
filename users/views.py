a849e42af11e15e70afe43f1d9604a1902ac44a7
danieliusvenc <danielius.vencius.30@gmail.com>
2025-10-28 11:01:58 +0000

    Add protected profile endpoint requiring JWT authentication

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
+
 
 User = get_user_model()
 
@@ -8,3 +12,10 @@ class RegisterView(generics.CreateAPIView):
   queryset = User.objects.all()
   serializer_class = RegisterSerializer
 
+
+class ProfileView(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def get(self, request):
+    user = request.user
+    return Response({"username": user.username})
